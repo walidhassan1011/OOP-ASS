d5b762b9c3f9fd341fb8f1579a547746e884455b
walidhassan1011 <walidhassan009@gmail.com>
2023-03-18 13:41:42 +0200

    ASS2

--- ASS2/src/Main.java
+++ ASS2/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+   // Question 6 -a
+        // the ouput will be A's no-arg constructor is invoked
+    // Question 6 -b
+        // the problem is that class A waiting for value in the constructor and class B doesn't
+        // have any value to pass to the constructor
+    //------------------------------------------------------------------------------------------
+
+    // Question 7
+        // class B extends A  so to override the method in class A
+        // we need to use the same method using super keyword to call the method in class A
+    //------------------------------------------------------------------------------------------
+
+    // Question 8
+        // override
+    //------------------------------------------------------------------------------------------
+
+    // Question 9
+        // yes
+    //------------------------------------------------------------------------------------------
+
+
 }
--- ASS2/src/MyInteger.java
+++ ASS2/src/MyInteger.java
@@ -1,2 +1,106 @@
 public class MyInteger {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public MyInteger(int value) {
+        MyInteger myInteger = new MyInteger(value);
+        myInteger.value = value;
+        myInteger.getValue();
+
+    }
+
+    public boolean isEven() {
+        if (value % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isOdd() {
+        if (value % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isPrime() {
+        for (int i = 2; i < value; i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isEven(int value) {
+        if (value % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isOdd(int value) {
+        if (value % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isPrime(int value) {
+        for (int i = 2; i < value; i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isEven(MyInteger myInteger) {
+        if (myInteger.value % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isOdd(MyInteger myInteger) {
+        if (myInteger.value % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isPrime(MyInteger myInteger) {
+        for (int i = 2; i < myInteger.value; i++) {
+            if (myInteger.value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(int value) {
+        if (this.value == value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean equals(MyInteger myInteger) {
+        if (this.value == myInteger.value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
